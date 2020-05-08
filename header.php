929c2dc10af25e78b305826058bfa9034666da95
Stabien <bastien.piedallu@gmail.com>
2020-05-08 14:49:07 +0200

    creation des pages categories pour les produits + ajout des liens des pages sur le header

---
+++ header.php
@@ -0,0 +1,23 @@
+<head>
+	<link rel="stylesheet" href="css/header.css"/>
+</head>
+
+<header>
+	<div id="research">
+		<a href="index.php">CYABA</a>
+		<input type="text" placeholder="Rechercher">
+		<input type="image" src="css/loupe.png">
+	</div>
+	<nav>
+		<ul>
+			<a href="#"><li>Nos Meilleures ventes</li></a>
+			<a href="#"><li>Nos promotions</li></a>
+			<a href="audiovisuel.php"><li>Audiovisuel</li></a>
+			<a href="informatique.php"><li>Informatique</li></a>
+			<a href="objets_connectes.php"><li>Objets connectés</li></a>
+			<a href="#"><li>Nous contacter</li></a>
+			<a href="#"><li>Connexion</li></a>
+			<a href="#"><li>Inscription</li></a>
+		</ul>
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" href="css/style.css"/>
+	<title>CYABA</title>
+</head>
+<body>
+	<?php include('header.php'); ?>
+	<div id="banner">
+		<img src="clavier.jpg">
+	</div>
+	<div id="promotions">
+		<h1>Nos promotions</h1>
+		<div id="image_promotions"></div>
+	</div>
+</body>
+</html>
