2f06d32d5436ea6e93bd7827f8c5b4af77df2b5a
Stabien <bastien.piedallu@gmail.com>
2020-05-25 21:55:16 +0200

    update page d'accueil

--- index.php
+++ index.php
@@ -8,24 +8,29 @@
 <body>
 	<?php include('header.php'); ?>
 	<div id="banner">
-		<img src="css/images/clavier.jpg">
+		<figure>
+			<img src="css/images/banniere_1.jpg">
+			<img src="css/images/banniere_2.png">
+			<img src="css/images/banniere_3.jpg">
+			<img src="css/images/banniere_1.jpg">
+		</figure>
 	</div>
 	<div class="trend_container">
 		<h2>Nos promotions</h2>
 		<div class="photos_produits">
-			<h3><img src="css/images/av_camera_camescope_jvc_1.jpg">camera camescope jvc</h3>
-			<h3><img src="css/images/av_photo_objectif_canon_1.jpg">Caméra</h3>
-			<h3><img src="css/images/av_enceinte_portable_jbl_1.jpg">Caméra</h3>
-			<h3><img src="css/images/info_ordinateur_portable_alienware_1.jpg">Caméra</h3>
+			<a href="detail.php?id=19"><img src="css/images/av_camera_camescope_jvc_1.jpg">JVC GY-HM180E<h5>-50%</h5></a>
+			<a href="detail.php?id=12"><img src="css/images/av_photo_objectif_canon_1.jpg">Canon EF 100 mm<h5>-30%</h5></a>
+			<a href="detail.php?id=27"><img src="css/images/av_enceinte_portable_jbl_1.jpg">JBL 104-BT Noir<h5>-40%</h5></a>
+			<a href="detail.php?id=36"><img src="css/images/info_ordinateur_portable_alienware_1.jpg">Alienware M15<h5>-35%</h5></a>
 		</div>
 	</div>
 	<div class="trend_container">
 		<h2>Nos meilleures ventes</h2>
 		<div class="photos_produits">
-			<h3><img src="css/images/oc_sport_drone_dji_1.jpg">Caméra</h3>
-			<h3><img src="css/images/info_pièce_graphique_asrock_1.jpg">Caméra</h3>
-			<h3><img src="css/images/av_photo_objectif_nikon_2.jpg">Caméra</h3>
-			<h3><img src="css/images/oc_maison_enceinte_google_1.jpg">Caméra</h3>
+			<a href="detail.php?id=73"><img src="css/images/oc_sport_drone_dji_1.jpg">DJI Mavic Mini</a>
+			<a href="detail.php?id=55"><img src="css/images/info_pièce_graphique_asrock_1.jpg">ASRock Phantom Radeon</a>
+			<a href="detail.php?id=16"><img src="css/images/av_photo_objectif_nikon_2.jpg">Nikon NIKKOR Z 14-30 mm</a>
+			<a href="detail.php?id=62"><img src="css/images/oc_maison_enceinte_google_1.jpg">Google Home</a>
 		</div>
 	</div>
 	<footer>
