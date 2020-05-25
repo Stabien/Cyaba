8fcbb9a2ce448e9ab464e1fc305eb4d646f15ecb
Stabien <bastien.piedallu@gmail.com>
2020-05-25 19:11:42 +0200

    update page d'accueil

--- index.php
+++ index.php
@@ -10,8 +10,26 @@
 	<div id="banner">
 		<img src="css/images/clavier.jpg">
 	</div>
-	<div id="promotions">
-		<div id="image_promotions"></div>
+	<div class="trend_container">
+		<h2>Nos promotions</h2>
+		<div class="photos_produits">
+			<h3><img src="css/images/av_camera_camescope_jvc_1.jpg">camera camescope jvc</h3>
+			<h3><img src="css/images/av_photo_objectif_canon_1.jpg">Caméra</h3>
+			<h3><img src="css/images/av_enceinte_portable_jbl_1.jpg">Caméra</h3>
+			<h3><img src="css/images/info_ordinateur_portable_alienware_1.jpg">Caméra</h3>
+		</div>
 	</div>
+	<div class="trend_container">
+		<h2>Nos meilleures ventes</h2>
+		<div class="photos_produits">
+			<h3><img src="css/images/oc_sport_drone_dji_1.jpg">Caméra</h3>
+			<h3><img src="css/images/info_pièce_graphique_asrock_1.jpg">Caméra</h3>
+			<h3><img src="css/images/av_photo_objectif_nikon_2.jpg">Caméra</h3>
+			<h3><img src="css/images/oc_maison_enceinte_google_1.jpg">Caméra</h3>
+		</div>
+	</div>
+	<footer>
+
+	</footer>
 </body>
 </html>
